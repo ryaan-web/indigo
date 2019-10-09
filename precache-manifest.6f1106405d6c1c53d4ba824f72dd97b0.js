e6d5c8ddf5d7e4e1e69349f0711518f209bdf069
Abhishek Kumar <abhishek.kumar@AbhishekKumar.local>
2019-10-09 08:45:15 +0530

    Updates

---
+++ precache-manifest.6f1106405d6c1c53d4ba824f72dd97b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc412a54b731f8fa4ca1caa3d329325",
+    "url": "/indigo/index.html"
+  },
+  {
+    "revision": "80b3c5f704f3c5958315",
+    "url": "/indigo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d3ef5dea88c89c3e9b1d",
+    "url": "/indigo/static/css/main.6ebeec99.chunk.css"
+  },
+  {
+    "revision": "80b3c5f704f3c5958315",
+    "url": "/indigo/static/js/2.6fa30b0d.chunk.js"
+  },
+  {
+    "revision": "d3ef5dea88c89c3e9b1d",
+    "url": "/indigo/static/js/main.2891acfb.chunk.js"
+  },
+  {
+    "revision": "ebb19fc601465de5a365",
+    "url": "/indigo/static/js/runtime-main.2a02bc7f.js"
+  }
+]);
